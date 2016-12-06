48304a3704200728247412db8566cfd0ebe01342
Armen Baghumian <abaghumian@noggin.com.au>
2016-12-06 15:58:40 +1100

    Test the list of nodes

--- index.php
+++ index.php
@@ -3,4 +3,7 @@
 $rt = gethostbyname(getenv('RT_INDEX_CLUSTER'));
 $dist = gethostbyname(getenv('DIST_INDEX_CLUSTER'));
 
-var_dump($_ENV, $rt, $dist);
+$rts = gethostbynamel(getenv('RT_INDEX_CLUSTER'));
+$dists = gethostbynamel(getenv('DISTS_INDEX_CLUSTER'));
+
+var_dump($_ENV, $rt, $dist, $rts, $dists);
